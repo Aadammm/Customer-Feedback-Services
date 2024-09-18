e2778a5c880c5c31f969bae7e36efa838ed764a4
janak <janak@ADAMOVINA>
2024-09-18 14:50:37 +0200

    #adding sorting by id and count documents in db

--- Program.cs
+++ Program.cs
@@ -34,14 +34,24 @@ public class FeedbackController : ControllerBase
 		_feedbackService = feedbackService;
 
 	[HttpGet]
-	public async Task<List<FeedBackModel>> Get() =>
-	await _feedbackService.GetAsync();
-
+	[Route("")]
+	[Route("page={pageNumber}&perPage={documentsLimitPerPage}")]
+	public Page Get(int pageNumber=1, int documentsLimitPerPage=10)
+	{
+		var totalDocuments = _feedbackService.CountDocuments();//nefunguje strankovanie 
+		var documents = _feedbackService.GetLimitedDocuments(pageNumber, documentsLimitPerPage).ToList();
+		var page = new Page()
+		{
+			TotalDocuments = totalDocuments,
+			Documents = documents
+		};
+		return page;
+	}
 
 	[HttpGet("{id:length(24)}")]
-	public async Task<ActionResult<FeedBackModel>> Get(string id)
+	public ActionResult<FeedBackModel> Get(string id)
 	{
-		var feedbackModel = await _feedbackService.GetByIdAsync(id);
+		var feedbackModel = _feedbackService.GetDocumentById(id);
 
 		if (feedbackModel is null)
 		{
@@ -51,17 +61,17 @@ public class FeedbackController : ControllerBase
 	}
 
 	[HttpPost]
-	public async Task<IActionResult> Post(FeedBackModel newFeedback)
+	public IActionResult Post(FeedBackModel newFeedback)
 	{
-		await _feedbackService.CreateAsync(newFeedback);
+		_feedbackService.CreateDocument(newFeedback);
 		return CreatedAtAction(nameof(Get), new { id = newFeedback.Id }, newFeedback);
 	}
 
 	[HttpPut]
 	[HttpPut("{id:length(24)}")]
-	public async Task<IActionResult> Update(string id, FeedBackModel updatedFeedback)
+	public IActionResult Update(string id, FeedBackModel updatedFeedback)
 	{
-		var feedback = await _feedbackService.GetByIdAsync(id);
+		var feedback = _feedbackService.GetDocumentById(id);
 		if (feedback is null)
 		{
 			return NotFound();
@@ -69,20 +79,20 @@ public class FeedbackController : ControllerBase
 
 		updatedFeedback.Id = feedback.Id;
 
-		await _feedbackService.UpdateAsync(id, updatedFeedback);
+		_feedbackService.Update(id, updatedFeedback);
 		return NoContent();
 	}
 
 	[HttpDelete("{id:length(24)}")]
-	public async Task<IActionResult> Delete(string id)
+	public IActionResult Delete(string id)
 	{
-		var feedback = _feedbackService.GetByIdAsync(id);
+		var feedback = _feedbackService.GetDocumentById(id);
 		if (feedback is null)
 		{
 			return NotFound();
 		}
 
-		await _feedbackService.RemoveAsync(id);
+		_feedbackService.RemoveAsync(id);
 		return NoContent();
 	}
 }
@@ -102,21 +112,29 @@ public class FeedBackService
 			feedBackDatabaseSettings.Value.CollectionName);
 	}
 
-	public async Task<List<FeedBackModel>> GetAsync() =>
-		await _feedbacksCollection.Find(FeedBackModel => true).ToListAsync();
+	public IEnumerable<FeedBackModel> GetDocuments() =>
+		 _feedbacksCollection.Find(FeedBackModel => true).ToList();
 
-	public async Task<FeedBackModel> GetByIdAsync(string id) =>
-		await _feedbacksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+	public IEnumerable<FeedBackModel> GetLimitedDocuments(int pageNumber, int documentsLimitPerPage)
+	{
+		return _feedbacksCollection.Find(FeedBackModel => true).Skip(pageNumber<2?0:pageNumber*documentsLimitPerPage).Limit(documentsLimitPerPage)
+			.SortBy(feedback => feedback.Id).ToList();
+	}
 
-	public async Task CreateAsync(FeedBackModel newFeedBackModel) =>
-		await _feedbacksCollection.InsertOneAsync(newFeedBackModel);
+	public FeedBackModel GetDocumentById(string id) =>
+		 _feedbacksCollection.Find(x => x.Id == id).FirstOrDefault();
 
-	public async Task UpdateAsync(string id, FeedBackModel updateFeedback) =>
-		await _feedbacksCollection.ReplaceOneAsync(x => x.Id == id, updateFeedback);
+	public void CreateDocument(FeedBackModel newFeedBackModel) =>
+		 _feedbacksCollection.InsertOne(newFeedBackModel);
 
-	public async Task RemoveAsync(string id) =>
-	await _feedbacksCollection.DeleteOneAsync(x => x.Id == id);
+	public void Update(string id, FeedBackModel updateFeedback) =>
+		_feedbacksCollection.ReplaceOne(x => x.Id == id, updateFeedback);
 
+	public void RemoveAsync(string id) =>
+	 _feedbacksCollection.DeleteOne(x => x.Id == id);
+
+	public long CountDocuments() =>
+		_feedbacksCollection.CountDocuments(new BsonDocument());
 
 }
 
@@ -139,14 +157,11 @@ public class FeedBackModel
 	public required string Product { get; set; }
 	public required string Vendor { get; set; }
 }
-//public class Page
-//{
-//	public long TotalDocuments
-//	{
-//		get; set;
-//	}
-//	public bool IsFirstPage { get; set; }
-//	public bool IsLastPage { get; set; }
-//	public List<FeedBackModel> Documents { get; set; }
+public class Page
+{
+	public long TotalDocuments { get; set; }
+	public bool IsFirstPage { get; set; } = true;
+	public bool IsLastPage { get; set; } = true;
+	public List<FeedBackModel> Documents { get; set; } = [];
 
-//}
+}
